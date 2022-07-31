cdc232aa1cc7b0986eef64258f06887d826dec8b
Naailaa <naila.rou123@gmail.com>
2022-07-31 02:09:26 +0100

    task2

--- faces-train.py
+++ faces-train.py
@@ -3,15 +3,23 @@ import os
 import numpy as np
 from PIL import Image
 import pickle
+import splitfolders 
+
+def norm(img):
+	norm_img = np.zeros((300, 300))
+	norm_img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
+	
+	return norm_img
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
+
+splitfolders.ratio(image_dir,'C:/Users/naila/OneDrive/Bureau/test/output/',seed=1337,ratio=(0.8,0.2))
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
-#dictionary
 label_ids = {}
 y_labels = []
 x_train = []
@@ -27,14 +35,9 @@ for root, dirs, files in os.walk(image_dir):
 				current_id += 1
 			id_ = label_ids[label]
 			
-			#y_labels.append(label) # some number
-			#x_train.append(path) # verify this image, turn into a NUMPY arrray, GRAY
 			pil_image = Image.open(path).convert("L") # grayscale
-			#resize the image
-			size = (550,550)
-			final_image = pil_image.resize(size,Image.ANTIALIAS)
-			#put it into array
-			image_array = np.array(final_image, "uint8")
+			image_array = np.array(pil_image, "uint8")
+			image_array = norm(image_array)
 			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
 			for (x,y,w,h) in faces:
